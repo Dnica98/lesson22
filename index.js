c3d4838e6b73d3dfe74f250ec8c40d17075e52a1
Dnica98 <daniela.nica98@gmail.com>
2024-10-25 14:35:20 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 
 
-const testDiv = document.getElementById('test')
+// const testDiv = document.getElementById('test')
+// const testDiv = document.getElementsByClassName('queryClass')
+// const testDiv = document.getElementsByTagName('div')
+// const testDiv = document.querySelector('#test')
+// const testDiv = document.querySelector('.queryClass')
+// const testDiv = document.querySelectorAll('#test')
+// const testDiv = document.querySelectorAll('.queryClass')
+
+// console.log(testDiv)
+
+// const elementList =Array.from(testDiv)
+
+// testDiv.classList.add('testClass')
+
+// testDiv[0].classList.add('testClass')
+// testDiv.classList.add('testClass')
+
 
